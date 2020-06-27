673dcdcead0e61e19a5004ccc90bb780a1916c50
Raj Negi <thecodersblock@gmail.com>
2020-06-27 15:25:57 +0530

    Final test

--- backend/logger.js
+++ backend/logger.js
@@ -1,15 +1,19 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
+const Sentry = require('@sentry/node');
 
 const logFormat = winston.format.printf((info) => {
   const date = new Date().toISOString();
-  const logString = info instanceof Error ?
-    JSON.stringify({
+  let logString = info.message;
+  if (info instanceof Error) {
+    if (process.env.NODE_ENV === 'production') {
+      Sentry.captureException(info);
+    }
+    logString = JSON.stringify({
       message: info.message,
       stack: info.stack,
-    }, null, 4).replace(/\\n/g, '\n')
-    :
-    info.message;
+    }, null, 4).replace(/\\n/g, '\n');
+  }
   return `${date}-${info.level}: ${logString}\n`;
 });
 
