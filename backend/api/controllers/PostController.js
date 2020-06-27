b0dfae4fd01af945725b01ab1680ffa6ad359d86
Raj Negi <thecodersblock@gmail.com>
2020-06-27 15:36:37 +0530

    Sentry works
    '
    '

--- backend/api/controllers/PostController.js
+++ backend/api/controllers/PostController.js
@@ -9,7 +9,6 @@ const logger = require('../../logger');
 const PostController = () => {
   const getAll = async (req, res) => {
     try {
-      tryingOnProudction
       const result = await Post.findAll({
         where: { published: true, title: { [Sequelize.Op.iLike]: `%${req.query.search || ''}%` } },
         order: [
