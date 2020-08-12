e285dac313cf1379f6d41ae37cafc7bcafa2198d
Raj Negi <thecodersblock@gmail.com>
2020-08-12 20:02:17 +0530

    Only google verified

--- backend/api/services/PostService.js
+++ backend/api/services/PostService.js
@@ -66,7 +66,7 @@ function similarity(s1, s2) {
 function optimizedDecorateListItem(post, currentUserId, comments, likes, hackathonId) {
   const likesCount = likes.filter((obj) => {
     if (hackathonId) {
-      return obj.postId === post.id && ['I2IT', 'International institute of information technology', 'I2it'].find((college) => obj.user.college && similarity(college, obj.user.college) > 0.6);
+      return post.user.viaGoogle && obj.postId === post.id && ['I2IT', 'International institute of information technology', 'I2it'].find((college) => obj.user.college && similarity(college, obj.user.college) > 0.6);
     }
     return obj.postId === post.id;
   }).length;
