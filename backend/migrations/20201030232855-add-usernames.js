ca155fde371b1718e0e090c40a2780d409ca0831
Raj Negi <thecodersblock@gmail.com>
2020-10-31 05:18:52 +0530

    Fix migrations

--- backend/migrations/20201030232855-add-usernames.js
+++ backend/migrations/20201030232855-add-usernames.js
@@ -1,16 +1,24 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 
+const { customAlphabet } = require('nanoid');
+
 const User = require('../api/models/User');
+
+const alphabet = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz';
+
+export default function randomId() {
+  return customAlphabet(alphabet, 9);
+}
 
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
     let tag = await User.update({
-      userName: `temp_${Math.random().toString(36).substr(2, 5)}`,
+      userName: `temp_${randomId()()}`,
     }, {
       returning: true,
       where: {
-        userName: null,
+        userName: 'temp_yisuh',
       },
     });
   },
