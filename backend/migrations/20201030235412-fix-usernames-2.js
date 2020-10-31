c94f88522166d80ecb83f3c7eea001ada0ac0bda
Raj Negi <thecodersblock@gmail.com>
2020-10-31 05:33:07 +0530

    Fix again

---
+++ backend/migrations/20201030235412-fix-usernames-2.js
@@ -0,0 +1,38 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
+
+import { post } from 'request';
+import { userSetter } from 'core-js/fn/symbol';
+
+const { customAlphabet } = require('nanoid');
+
+const User = require('../api/models/User');
+
+const alphabet = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz';
+
+export default function randomId() {
+  return customAlphabet(alphabet, 9);
+}
+
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    let tag = await User.findAll({
+      where: {
+        userName: 'temp_5KySMfNFX0',
+      },
+    }).then(async (users) => {
+      users.forEach(async (user) => {
+        const result = await user.update({ userName: `temp_${randomId()()}` });
+      });
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
